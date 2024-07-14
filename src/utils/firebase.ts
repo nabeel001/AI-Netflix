83f68b84c55129837f23a529c4ebc13e8b8af2a4
Nabeel Hussain <mnnh2001@gmail.com>
2024-07-14 16:07:06 +0530

    initial web app hosting to firebase

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -18,4 +18,5 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const analytics = getAnalytics(app);
